fb21d2b47a8b76466ab0fd2df11331fba7781e95
sneha <snehasreeramini@gmail.com>
2023-05-19 07:04:20 +0200

    Making Frontend

---
+++ roboshop/components/conditions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# case and if are conditiona; commands,if command is widly used
+#because it has more options
+
+#if condition found in 3 forms
+
+#simple if
+#if [expression]
+#then
+#commands
+#f1
+
+if[ 1 -eq 1]
+then
+    echo hello
+f1
--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
+USER_ID=$(id -u)
+if ["$USER_ID" -ne 0]; then
+   echo you should run your script as sudo root user
+   exit 1
+fi
+
+echo -e "\e[36m Installing Nginx \e[0m"
 yum install nginx -y
+if [$? -eq 0]; then
+  echo  -e "\e[32mSUCCESS\e[0m"
+else
+  echo "\e[32mFAILURE\e[0m"
+  exit 1
+fi
+
+
 systemctl enable nginx
 systemctl start nginx
 
 #Let's download the HTDOCS content and deploy under the Nginx path.
-
+echo -e "\e[36m Downloading Nginx content \e[0m"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+if [$? -eq 0]; then
+  echo "\e[32mSUCCESS\e[0m"
+else
+  echo "\e[32mFAILURE\e[0m"
+  exit 1
+fi
 
 #Deploy in Nginx Default Location.
+echo -e "\e[36m Cleaning Old Nginx Content and extract the downloaded new content \e[0m"
 rm -rf /usr/share/nginx/html/*
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
@@ -17,7 +39,21 @@ mv static/* .
 rm -rf frontend-master README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 
-#Finally restart the service once to effect the changes.
+if [$? -eq 0]; then
+  echo "\e[32mSUCCESS\e[0m"
+else
+  echo "\e[32mFAILURE\e[0m"
+  exit 1
+fi
 
+#Finally restart the service once to effect the changes.
+echo -e "\e[36m Starting Nginx \e[0m"
 systemctl restart nginx
 systemctl enable nginx
+
+if [$? -eq 0]; then
+  echo "\e[32mSUCCESS\e[0m"
+else
+  echo "\e[32mFAILURE\e[0"
+  exit 1
+fi
