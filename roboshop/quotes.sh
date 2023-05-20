7b8dbbed96c02f92d24ea394ea6cdef3407dd5bd
sneha <snehasreeramini@gmail.com>
2023-05-20 19:46:25 +0200

    quotes

---
+++ roboshop/quotes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo 'Apples cost is $100'
+echo "Apples cost is $100"
+
+TRAINING=DevOps
+echo 'Training=${TRAINING}'
+echo "Training=${TRAINING}"
+
+#no change will be done in output in single quotes
+#any varaiable to be considered use double quotes
