ca07cfbafa64c4ced139cb9c3d5e3cc424c68890
sneha <snehasreeramini@gmail.com>
2023-05-22 10:28:18 +0200

    Add Logs to frontend.sh Add mongodb file

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -17,30 +17,33 @@ if ["$USER_ID" -ne 0]; then
    exit 1
 fi
 
+LOG_FILE=/tmp/roboshop.log
+rm -f $LOG_FILE
+
 Print "Installing Nginx"
-yum install nginx -y
+yum install nginx -y &>>LOG_FILE
 StatCheck $?
 
 #Let's download the HTDOCS content and deploy under the Nginx path.
 Print "Downloading Nginx content"
-curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"&>>$LOG_FILE
 StatCheck $?
 
 #Deploy in Nginx Default Location.
-Print "Cleaning Old Nginx Content and extract the downloaded new content "
+Print "Cleaning Old Nginx Content and extract the downloaded new content "&>>LOG_FILE
 rm -rf /usr/share/nginx/html/*
 StatCheck $?
 
 cd /usr/share/nginx/html
 
-Print "Extracting archive"
+Print "Extracting archive"&>>$LOG_FILE
 unzip /tmp/frontend.zip && mv frontend-main/* .&& mv static/* .
 #logical and tells all commands to be executed
 #local or tells to execute any one of the command
 StatCheck $?
 
 rm -rf frontend-master README.md
-Print "Update Roboshop Configuration"
+Print "Update Roboshop Configuration"&>>$LOG_FILE
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 StatCheck $?
 #Finally restart the service once to effect the changes.
