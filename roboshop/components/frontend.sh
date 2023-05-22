b0b51aac68812f86c1f73004ec8814cf5b631117
sneha <snehasreeramini@gmail.com>
2023-05-22 04:54:54 +0200

    make changes with functions statcheck

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -8,23 +8,26 @@ else
   exit 2
 fi
 }
+Print(){
+  echo -e "\e[36m $1 \e[0m"
+}
 USER_ID=$(id -u)
 if ["$USER_ID" -ne 0]; then
    echo you should run your script as sudo root user
    exit 1
 fi
 
-echo -e "\e[36m Installing Nginx \e[0m"
+Print "Installing Nginx"
 yum install nginx -y
 StatCheck $?
 
 #Let's download the HTDOCS content and deploy under the Nginx path.
-echo -e "\e[36m Downloading Nginx content \e[0m"
+Print "Downloading Nginx content"
 curl -f -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 StatCheck $?
 
 #Deploy in Nginx Default Location.
-echo -e "\e[36m Cleaning Old Nginx Content and extract the downloaded new content \e[0m"
+Print "Cleaning Old Nginx Content and extract the downloaded new content "
 rm -rf /usr/share/nginx/html/*
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
@@ -35,7 +38,7 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf
 StatCheck $?
 #Finally restart the service once to effect the changes.
 
-echo -e "\e[36m Starting Nginx \e[0m"
+Print" Starting Nginx "
 systemctl restart nginx
 StatCheck $?
 systemctl enable nginx
