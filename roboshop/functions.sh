6a0074fdd87e1a31237f1c5dbc32a70fe38da00e
sneha <snehasreeramini@gmail.com>
2023-05-22 04:08:57 +0200

    functions

---
+++ roboshop/functions.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#variable:name to set of data
+#function:name to set of commands
+
+#func_name() {
+  #commands
+  #commands
+  #}
+  #func_name
+
+  ##Declare a function
+Print_Message() {
+   echo Hello,
+   echo GoodMorning
+   echo Welcome to Devops Training
+}
+ Print_Message
