3079d1b6c1fc57ca6b4304789dac2be4e66c4653
sneha <snehasreeramini@gmail.com>
2023-05-26 16:24:21 +0200

    Create ec2 instance changes in common.sh modifications in payments.sh Add dispatch.sh

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-practice" | jq '.Images[].ImageId')
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId')
 
 echo $AMI_ID
+
+
