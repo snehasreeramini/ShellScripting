434a955e3806990b5001171757ff43f81b837d4a
sneha <snehasreeramini@gmail.com>
2023-05-27 10:02:47 +0200

    modifications to create ec2.sh

--- create-ec2.sh
+++ create-ec2.sh
@@ -9,7 +9,6 @@ COMPONENT=$1
 ZONE_ID=Z05207852YRR0B2XTDV6K
 
 create_ec2() {
-echo $AMI_ID
 PRIVATE_IP=$(aws ec2 run-instances \
 --image-id ${AMI_ID} \
 --instance-type t2.micro \
@@ -24,9 +23,9 @@ aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-bat
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g' )
 SGID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=Allow-all-from-public | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')
 
-if ["$1" == "all" ]; then
+if [ "$1" == "all" ]; then
  for component in catalogue cart user redis rabbitmq user payments frontend mongodb mysql ; do
-   COMPONENT= $component
+   COMPONENT=$component
    create_ec2
    done
   else
